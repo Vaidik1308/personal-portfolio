d0ede0afe3625bffa80ce58875dce8151aad7716
vaidik <vaidiksinghnirwan@gmail.com>
2024-10-21 18:54:50 +0530

    implemented heros section

--- src/app/components/Home/HeroSection.tsx
+++ src/app/components/Home/HeroSection.tsx
@@ -19,21 +19,25 @@ const HeroSection = () => {
         <Wrapper className='md:w-[80%] mx-auto flex md:flex-row flex-col relative px-8'>
             <div className='  md:w-[90%] flex justify-center items-start  h-[70vh] flex-col'>
                 <div className='md:w-[60%] mx-auto flex flex-col gap-5 justify-center w-full'>
-                    <h1 className='flex flex-col text-4xl md:text-7xl w-[90%] leading-[3.5rem] md:leading-[4rem]'>
+                    <h1 className='flex flex-col text-4xl md:text-7xl w-[90%] leading-[2rem] md:leading-[4rem]'>
                         <span>Hi</span>
                         {/* <span>I'm Vaidik</span> */}
                         <div className='flex flex-wrap  items-end'>
-                          <span className='  flex items-end '>
+                          <span className='  flex items-end md:mb-0 mb-2'>
                             I'm a  
                           </span>
                           <div className='font-Playwrite font-[900] w-fit'>
                             <LetterAnimation text='Frontend' /> 
                           </div>  
                           <motion.div 
+                            initial={{
+                              x:100
+                            }}
                             animate={{
                               x:-15,
                             }} 
                             transition={{
+                              type:"spring",
                               delay:1
                             }}
                             className='font-Playwrite font-[900] w-fit'>
@@ -42,7 +46,17 @@ const HeroSection = () => {
                           </motion.div>  
                         </div>
                     </h1>
-                    <div className='grid grid-cols-4 items-center gap-4 text-3xl  w-fit  inset-4 px-5 py-3 rounded-lg bg-gray-800' >
+                    <motion.div 
+                      className='grid grid-cols-4 items-center gap-4 text-3xl  w-fit  inset-4 px-5 py-3 rounded-lg bg-gray-800' 
+                      initial={{ scale: 0 }}
+                          animate={{ rotate: 0, scale: 1 }} // Changed to 360 degrees
+                          transition={{
+                            type: "spring",
+                            stiffness: 260,
+                            damping: 20,
+                            delay: 0.1 // Delay for the first icon
+                          }}
+                    >
                         <motion.div
                           className="w-full flex items-center justify-center"
                           initial={{ scale: 0 }}
@@ -88,14 +102,7 @@ const HeroSection = () => {
                               <FaSquareXTwitter size={40} />
                           </Link>
                         </motion.div>
-                        <motion.button
-                          whileHover={{
-                              scale: 1.2,
-                              transition: { duration: 0.1 },
-                              rotate: 45
-                          }}
-                          whileTap={{ scale: 0.9 }}
-                        >
+                        
                           <motion.div
                             className="flex items-center justify-center"
                             initial={{ scale: 0 }}
@@ -111,13 +118,26 @@ const HeroSection = () => {
                                 <FaGithub size={35} />
                             </Link>
                           </motion.div>
-                        </motion.button>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
-            <div className='w-[45%] md:flex hidden absolute right-0'>
-                <Image src={ProfilePic} width={500} height={500} className='w-[100%] h-auto' alt='profilePic'/>
+            <div className='w-[45%] md:flex hidden absolute z-10 -right-[5%]'>
+                <Image src={ProfilePic} width={500} height={500} className='w-[100%] h-auto ' alt='profilePic'/>
             </div>
+            <motion.div 
+              initial={{
+                x:100,
+                opacity:0
+              }}
+              animate={{
+                x:0,
+                opacity:1
+              }}
+              transition={{
+                type:"spring",
+                delay:1
+              }}
+              className='absolute size-[25rem] z-[5] bg-green-700 right-[0%] bottom-[5%] rounded-full' />
         </Wrapper>
     </section>
   )
--- src/app/components/Reusable/LetterAnimation.tsx
+++ src/app/components/Reusable/LetterAnimation.tsx
@@ -7,15 +7,29 @@ interface LetterAnimationProps {
 }
 
 const LetterAnimation: React.FC<LetterAnimationProps> = ({ text }) => {
+  // Define an array of colors
+  const colors = [
+    'text-red-500',
+    'text-blue-500',
+    'text-green-500',
+    'text-yellow-500',
+    'text-purple-500',
+    'text-pink-500',
+    'text-orange-500',
+    'text-teal-500',
+    'text-indigo-500',
+    'text-gray-500',
+  ];
+
   return (
     <div className="flex p-3">
-      { text.split('').map((letter, index) => (
+      {text.split('').map((letter, index) => (
         <motion.span
           key={index}
           initial={{ opacity: 0, y: 20 }} // Start off-screen
           animate={{ opacity: 1, y: 0 }} // Animate to visible
           transition={{ duration: 0.5, delay: index * 0.1 }} // Staggered delay
-          className="inline-block" // Ensure letters are inline
+          className={`inline-block ${colors[index % colors.length]}`} // Assign color based on index
         >
           {letter}
         </motion.span>
