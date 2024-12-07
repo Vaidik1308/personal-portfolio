9cb8d5b5005cdf632492e1cd9370fd22bd5762e9
vaidik <vaidiksinghnirwan@gmail.com>
2024-12-07 18:20:10 +0530

    added the animation in the logo

--- src/components/Reusable/Logo.tsx
+++ src/components/Reusable/Logo.tsx
@@ -1,17 +1,24 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
-import BoyIcon from '../../../public/3d_boy.png'
+import BoyIcon from '../../../public/3d_boy.png';
 
 const Logo = () => {
+  console.log("Vaidik".split(""));
+  
   return (
-    <div className='text-2xl font-[500] flex items-center gap-2'>
-        <Image src={BoyIcon} width={40} height={40} alt='boy_3d'/>
-        <h3 className='uppercase'>
-            Vaidik. 
-        </h3>
+    <div className="text-2xl font-[500] flex items-center gap-2">
+      <Image src={BoyIcon} width={40} height={40} alt="boy_3d" />
+      <h3 className="uppercase font-extrabold flex gap-0.5 motion-scale-in-50 motion-preset-slide-right-lg">
+        <div className=' -motion-rotate-loop-12 motion-translate-y-loop-[20%] motion-duration-2000 motion-ease-in-out motion-delay-100'>V</div>
+        <div className='motion-translate-y-loop-[20%] motion-duration-2000 motion-ease-in-out motion-delay-200'>a</div>
+        <div className='motion-translate-y-loop-[20%] motion-duration-2000 motion-ease-in-out motion-delay-300'>i</div>
+        <div className='motion-translate-y-loop-[20%] motion-duration-2000 motion-ease-in-out motion-delay-500'>d</div>
+        <div className='motion-translate-y-loop-[20%] motion-duration-2000 motion-ease-in-out motion-delay-700'>i</div>
+        <div className='motion-translate-y-loop-[20%] motion-duration-1500 motion-ease-in-out motion-delay-1000'>k</div>
+      </h3>
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
