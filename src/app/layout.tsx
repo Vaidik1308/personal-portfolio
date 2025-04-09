86e67ef0f2b37f9fa2d10481d0f7695948d39f6b
vaidik <vaidiksinghnirwan@gmail.com>
2025-04-09 19:52:47 +0530

    fix unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Common/Header";
-import Footer from "@/components/Common/Footer";
+// import Footer from "@/components/Common/Footer";
 
 export const metadata: Metadata = {
   title: "Vaidik Singh Nirwan | Home",
